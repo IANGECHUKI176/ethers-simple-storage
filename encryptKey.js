08bd0b127f69b6c528014cff58b105b590cd74bb
IAN GECHUKI <iangechuki@gmail.com>
2022-08-01 19:48:40 +0300

    first commit

--- encryptKey.js
+++ encryptKey.js
@@ -1,22 +1,21 @@
 const ethers = require("ethers")
-const fs = require("fs-extra")
+const fs = require("fs")
 require("dotenv").config()
 
-async function main() {
-    console.log(process.env.PRIVATE_KEY)
-    console.log(process.env.PRIVATE_KEY_PASSWORD)
+const main = async () => {
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
     const encryptedJsonKey = await wallet.encrypt(
         process.env.PRIVATE_KEY_PASSWORD,
         process.env.PRIVATE_KEY
     )
-    console.log(encryptedJsonKey)
-    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey)
+    fs.writeFileSync("./.encryptedKey.json",encryptedJsonKey)
+  
 }
-
 main()
-    .then(() => process.exit(0))
+    .then(() => {
+        process.exit(0)
+    })
     .catch((error) => {
-        console.error(error)
+        console.log(error)
         process.exit(1)
     })
